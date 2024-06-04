ada8028205712662d0baae229ec6a2e7553ac908
ZachariahKB <zachariahakb@gmail.com>
2024-06-04 12:59:22 -0700

    client

--- client/src/utils/auth.js
+++ client/src/utils/auth.js
@@ -1,20 +1,23 @@
 import decode from 'jwt-decode';
 
 class AuthService {
+  // Decode token and get user profile
   getProfile() {
     return decode(this.getToken());
   }
 
+  // Check if user is logged in
   loggedIn() {
     const token = this.getToken();
     // If there is a token and it's not expired, return `true`
-    return token && !this.isTokenExpired(token) ? true : false;
+    return token && !this.isTokenExpired(token);
   }
 
+  // Check if the token is expired
   isTokenExpired(token) {
-    // Decode the token to get its expiration time that was set by the server
+    // Decode the token to get its expiration time that was set
     const decoded = decode(token);
-    // If the expiration time is less than the current time (in seconds), the token is expired and we return `true`
+    // If the expiration time is less than the current time (in seconds), the token is expired
     if (decoded.exp < Date.now() / 1000) {
       localStorage.removeItem('id_token');
       return true;
@@ -23,15 +26,18 @@ class AuthService {
     return false;
   }
 
+  // Get token from local storage
   getToken() {
     return localStorage.getItem('id_token');
   }
 
+  // Login user by saving the token to local storage
   login(idToken) {
     localStorage.setItem('id_token', idToken);
     window.location.assign('/');
   }
 
+  // Logout user by removing the token from local storage and reloading the page
   logout() {
     localStorage.removeItem('id_token');
     window.location.reload();

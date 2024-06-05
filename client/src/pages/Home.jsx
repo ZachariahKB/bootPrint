e130ff78dada508a405e75f357da36254af1510a
ZachariahKB <zachariahakb@gmail.com>
2024-06-04 18:16:58 -0700

    pauleegetdown

--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -12,7 +12,7 @@ const Home = () => {
   return (
     <main>
       <div className="flex-row justify-center">
-        hello
+        BOOM
         {/* <div
           className="col-12 col-md-10 mb-3 p-3"
           style={{ border: '1px dotted #1a1a1a' }}

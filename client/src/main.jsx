aa9b56ba15ba2d2a11223fd9e7c77f4c8b6456c2
ZachariahKB <zachariahakb@gmail.com>
2024-06-06 11:35:39 -0700

    paulee

--- client/src/main.jsx
+++ client/src/main.jsx
@@ -36,7 +36,7 @@ const router = createBrowserRouter([
        },
       {
         path: '/projects/:projectId',
-        element: <SingleThought />
+        element: <SingleProject />
       }
     ]
   },

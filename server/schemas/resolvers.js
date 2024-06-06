453bd9a48c7c9df5f6264adab9e500506d64d7a1
gitongah <mbaabugitonga@gmail.com>
2024-06-06 14:21:24 -0700

    Worked on the project comments adding resources and geting the resources by a the topics its under

--- server/schemas/resolvers.js
+++ server/schemas/resolvers.js
@@ -3,21 +3,31 @@ const { signToken, AuthenticationError } = require('../utils/auth');
 
 const resolvers = {
   Query: {
+    //find all the users with the projects they have
     users: async () => {
       return User.find().populate('projects');
     },
+    // finding one user and the projects
     user: async (parent, { username }) => {
       return User.findOne({ username }).populate('projects');
     },
+    // get all the projects
     projects: async (parent, { username }) => {
       const params = username ? { username } : {};
       return Project.find(params).sort({ createdAt: -1 });
-    //TODO: Create a single project if we have time
     },
-    resources: async (parent, { username })=>{
-      const params = username ? { username }: {};
+    //get a single project
+    project: async( parent, { projectId }) => {
+      return Project.findOne({_id:  projectId})
+    },
+    // get the resources
+    resources: async (parent, { topic })=>{
+      const params = topic ? {  topic}: {};
       return Resources.find(params).sort({createdAt: -1 });
     },
+    // TODO discus do we really need to get one resource???
+    //TODO discus  geting a single 
+
     me: async (parent, args, context) => {
       if (context.user) {
         return User.findOne({ _id: context.user._id }).populate('projects');
@@ -27,11 +37,13 @@ const resolvers = {
   },
 
   Mutation: {
+    //adding a new user
     addUser: async (parent, { username, email, password }) => {
       const user = await User.create({ username, email, password });
       const token = signToken(user);
       return { token, user };
     },
+    //loging in to the application
     login: async (parent, { email, password }) => {
       const user = await User.findOne({ email });
 
@@ -49,6 +61,7 @@ const resolvers = {
 
       return { token, user };
     },
+    //adding a project 
     addProject: async (parent, { description, title, githubRepo, contactInfo }, context) => {
       if (context.user) {
         const project = await Project.create({
@@ -69,6 +82,7 @@ const resolvers = {
       throw AuthenticationError;
       ('You need to be logged in!');
     },
+    //adding resources 
     addResource: async (parent, { topic, content }, context) => {
       if (context.user) {
         const resources = await Resources.create({
@@ -87,6 +101,25 @@ const resolvers = {
       throw AuthenticationError;
       ('You need to be logged in!');
     },
+    //adding a comment to the project
+    addComment: async (parent, { projectId, commentText }, context) => {
+      if (context.user) {
+        return Project.findOneAndUpdate(
+          { _id: projectId },
+          {
+            $addToSet: {
+              comments: { commentText, commentAuthor: context.user.username },
+            },
+          },
+          {
+            new: true,
+            runValidators: true,
+          }
+        );
+      }
+      throw AuthenticationError;
+    },
+    //removing a project you created from you application
     removeProject: async (parent, { projectId }, context) => {
       if (context.user) {
         const project = await Project.findOneAndDelete({
@@ -103,12 +136,17 @@ const resolvers = {
       }
       throw AuthenticationError;
     },
+    //remove a resource 
     removeResource: async (parent, { resourcesId }, context) => {
       if (context.user) {
+        console.log(resourcesId)
+        console.log(context.user.username)
         const resources = await Resources.findOneAndDelete({
+
           _id: resourcesId,
-          resourcesAuthor: context.user.username,
+          // resourcesAuthor: context.user.username,
         });
+        console.log(resources)
 
         await User.findOneAndUpdate(
           { _id: context.user._id },
@@ -116,6 +154,24 @@ const resolvers = {
         );
 
         return resources;
+      }
+      throw AuthenticationError;
+    },
+    // removing a comment
+    removeComment: async (parent, { projectId, commentId }, context) => {
+      if (context.user) {
+        return Project.findOneAndUpdate(
+          { _id: projectId },
+          {
+            $pull: {
+              comments: {
+                _id: commentId,
+                commentAuthor: context.user.username,
+              },
+            },
+          },
+          { new: true }
+        );
       }
       throw AuthenticationError;
     },

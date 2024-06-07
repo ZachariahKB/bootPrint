33bedecda5859762aa093961648988f5e334dfcf
gitongah <mbaabugitonga@gmail.com>
2024-06-07 02:30:36 -0700

    added the queries and mutation to be used in the frontend

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -1,5 +1,6 @@
 import { gql } from '@apollo/client';
 
+//the loging functionality
 export const LOGIN_USER = gql`
   mutation login($email: String!, $password: String!) {
     login(email: $email, password: $password) {
@@ -11,7 +12,7 @@ export const LOGIN_USER = gql`
     }
   }
 `;
-
+// the sign up of a new user
 export const ADD_USER = gql`
   mutation addUser($username: String!, $email: String!, $password: String!) {
     addUser(username: $username, email: $email, password: $password) {
@@ -23,24 +24,77 @@ export const ADD_USER = gql`
     }
   }
 `;
-
+//adding a new project. providing title githubRepo, a project description and contact info
 export const ADD_PROJECT = gql`
 mutation AddProject($title: String!, $description: String!, $githubRepo: String!, $contactInfo: String!) {
   addProject(title: $title, description: $description, githubRepo: $githubRepo, contactInfo: $contactInfo) {
-    contactInfo
-    description
-    githubRepo
     title
+    githubRepo
+    description
+    contactInfo
   }
 }
 `
+
+ // adding a resource to the application by providing the topic and content
+ export const ADD_RESOURCE =gql`
+  mutation AddResource($topic: String!, $content: String!) {
+    addResource(topic: $topic, content: $content) {
+      topic
+      content
+      createdAt
+    }
+  }
+ `;
+
+ //adding a comment to a single project
+ export const ADD_COMMENT = gql`
+  mutation AddComment($projectId: ID!, $commentText: String!) {
+    addComment(projectId: $projectId, commentText: $commentText) {
+      comments {
+        commentText
+        commentAuthor
+        createdAt
+      }
+    }
+  }
+ `;
+ //removing one resource using the resourceId
+ export const REMOVE_RESOURCE = gql`
+  mutation RemoveResource($resourcesId: ID!) {
+    removeResource(resourcesId: $resourcesId) {
+      content
+      createdAt
+      topic
+    }
+  }
+ `;
+
+//removing a comment from the project
+export const REMOVE_COMMENT =gql`
+  mutation RemoveComment($projectId: ID!, $commentId: ID!) {
+    removeComment(projectId: $projectId, commentId: $commentId) {
+      comments {
+        commentText
+      }
+      _id
+    }
+  }
+`;
+
+ //removing the project
 export const REMOVE_PROJECT = gql`
 mutation RemoveProject($projectId: ID!) {
   removeProject(projectId: $projectId) {
     title
+    description
+    githubRepo
   }
 }
 `;
+
+
+
 
 
 
--- client/src/utils/queries.js
+++ client/src/utils/queries.js
@@ -7,20 +7,53 @@ export const QUERY_USER = gql`
       username
       email
     }
-  }
-`;
-
-export const QUERY_PROJECTS= gql`
-query Projects {
-  projects {
-    contactInfo
-    description
-    githubRepo
-    title
+  me {
+    projects {
+      _id
+      description
+      createdAt
+    }
   }
 }
 `;
 
+//getting a list of all the users
+export const QUERY_ALL_USER =gql`
+  query Users {
+  users {
+    username
+  }
+}
+`;
+//getting the projects list with all the comments
+export const QUERY_PROJECTS= gql`
+query Projects {
+  projects {
+    title
+    description
+    githubRepo
+    contactInfo
+    projectAuthor
+    createdAt
+    comments {
+      commentAuthor
+      commentText
+      createdAt
+    }
+  }
+}
+`;
+//getting a single project with the project description, title and project author
+export const QUERY_SINGLE_PROJECT =gql`
+query Project($projectId: ID!) {
+  project(projectId: $projectId) {
+    description
+    title
+    projectAuthor
+  }
+}
+`;
+//geting the current user and the projects the have
 export const QUERY_ME = gql`
   query me {
     me {
@@ -36,3 +69,17 @@ export const QUERY_ME = gql`
     }
   }
 `;
+
+// getting  all the resources in the database. if the user wants to  get a certain 
+//topic they can search using that topic
+export const QUERY_RESOURCES = gql`
+  query Resources($topic: String) {
+    resources(topic: $topic) {
+      topic
+      content
+      createdAt
+    }
+  }
+`;
+
+
